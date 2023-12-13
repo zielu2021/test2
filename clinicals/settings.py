c9e3449e58508ee696e1cf900d10e5b118b6e5c2
Szczepan <szczepan.zielinski05@gmail.com>
2023-12-13 18:20:52 +0100

    commit1

--- clinicals/settings.py
+++ clinicals/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ STATIC_URL= BASE_DIR / 'static'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-43--kd(5ll5+8c-l#9be@@=s9$a_(69c1@n^q&#2)-wc+2sq62'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,9 +82,9 @@ WSGI_APPLICATION = 'clinicals.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clinicalsdb',
-        'USER': 'root',
-        'PASSWORD': 'Sapsp2014!'
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS')
     }
 }
 
