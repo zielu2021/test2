b08238910af78d15f272ca1e2d9fa54972536e9a
Szczepan <szczepan.zielinski05@gmail.com>
2023-12-13 16:04:50 +0100

    first commit

---
+++ clinicalsApp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from clinicalsApp.models import ClinicalData,Patient
+
+class PatientForm(forms.ModelForm):
+	class Meta:
+		model= Patient
+		fields= '__all__'
+
+class ClinicalDataForm(forms.ModelForm):
+	class Meta:
+		model=ClinicalData
+		fields='__all__'
+		
